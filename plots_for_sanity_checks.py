908c8377f52cc259d54791a4bf07c4a5ffef9bf0
ThePerson2 <Bouch123@umn.edu>
2021-08-16 14:53:35 +0200

    Update plots_for_sanity_checks.py

--- plots_for_sanity_checks.py
+++ plots_for_sanity_checks.py
@@ -26,7 +26,16 @@ def plot_pupil_size(data_to_analyze,session_id=0,neuron_id=10):
   plt.plot(pupil_data_to_plot_2)
   plt.show()
   plt.close()  
-  
+def plot_prediction_vs_data(rpred, x0, neuron_id=0, trial_id=29):
+  x0_rates = x0.detach().cpu().numpy()
+  plt.figure(figsize=(10, 6))
+  plt.plot(x0_rates[:,trial_id, neuron_id])
+  plt.plot(rpred[:,trial_id, neuron_id])
+  # plt.plot(-.5 + x[:, nn, 0]/4) # these are spikes we don't have that
+
+  plt.legend(['rates (true)', 'rates (predicted)', 'spikes'])
+  plt.title(f'Neuron {nn}')
+  plt.show()
   
   
   
